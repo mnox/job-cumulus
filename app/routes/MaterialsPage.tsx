7be7c2c7df29b33a317cc687b193fa65942b7c7e
Mnox <matt.noxon@lendio.com>
2025-05-20 16:11:59 -0600

    Update MaterialsPage.tsx

--- app/routes/MaterialsPage.tsx
+++ app/routes/MaterialsPage.tsx
@@ -1,8 +1,16 @@
 import Grid from '@mui/material/Grid';
 import * as React from 'react';
+import { useEffect } from 'react';
 import MaterialsTable from '~/components/materials/MaterialsTable';
+import { fetchMaterials } from '~/data/materials/Materials.slice';
+import { useAppDispatch } from '~/data/store/root-hooks';
 
 export default function MaterialsPage() {
+  const dispatch = useAppDispatch();
+  useEffect( () => {
+    dispatch(fetchMaterials(false));
+  }, [] );
+  
   return (
     <Grid size={'grow'}>
       <MaterialsTable />
