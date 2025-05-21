fe69cf691a86ae9674e2602867eeb8d0555b0963
Mnox <matt.noxon@lendio.com>
2025-05-21 09:40:26 -0600

    Update routes.ts

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,7 @@ import { index, route, type RouteConfig } from "@react-router/dev/routes";
 
 export default [
   index('./routes/Dashboard.tsx'),
-  route('customers', './routes/CustomersPage.tsx'),
+  route('customers', './routes/customers/CustomersPage.tsx'),
   route('jobs', './routes/JobsPage.tsx'),
   route('materials', './routes/MaterialsPage.tsx'),
   route('reports', './routes/ReportsPage.tsx'),
